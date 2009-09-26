6e9c96e8f1212ad001ec857b770a1ee6e924a9f0
dgoemans <dgoemans@d48d90ca-aac1-11de-b87c-2b1544cb6d8d>
2009-09-26 17:32:12 +0000

    initial code add

---
+++ trunk/src/com/davidgoemans/goosewidget/GooseWidget.java
@@ -0,0 +1,15 @@
+package com.davidgoemans.goosewidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class GooseWidget extends AppWidgetProvider 
+{
+ 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) 
+ 	{
+        context.startService(new Intent(context, UpdateService.class));
+    }
+}
