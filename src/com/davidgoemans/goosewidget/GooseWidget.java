874864f7da3852a0d12ced5e3930a3256b705576
dgoemans <dgoemans@d48d90ca-aac1-11de-b87c-2b1544cb6d8d>
2009-10-13 19:07:54 +0000

    Next/Prev and some aspect ratio fixing.

--- src/com/davidgoemans/goosewidget/GooseWidget.java
+++ src/com/davidgoemans/goosewidget/GooseWidget.java
@@ -4,11 +4,27 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 
 public class GooseWidget extends AppWidgetProvider 
 {
 	public static final String PREFS_NAME = "gooseprefs";
+
+//	@Override
+//	public void onEnabled(Context context) 
+//	{
+//		/*
+//        final Button button = (Button) findViewById(R.id.zin);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Perform action on click
+//            }
+//        });
+//        */
+//		
+//	}
 	
  	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) 
  	{
