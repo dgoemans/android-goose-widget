928a3e1128df0f5754cbbd83ef9feb90ee79aa9c
dgoemans <dgoemans@d48d90ca-aac1-11de-b87c-2b1544cb6d8d>
2009-10-24 16:54:35 +0000

    Fixed Viewer Added Older/Newer

--- src/com/davidgoemans/goosewidget/GooseMenu.java
+++ src/com/davidgoemans/goosewidget/GooseMenu.java
@@ -1,42 +1,53 @@
 package com.davidgoemans.goosewidget;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ListActivity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore.Images.Media;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 
 public class GooseMenu extends ListActivity
 {		
-	private String[] menuEntries;
+	private List<String> menuEntries;
+	private int page;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
-	{		
-		menuEntries = new String[7];
-		menuEntries[0] = getString(R.string.menu_ref);
+	{
+		menuEntries = new ArrayList<String>();
+		menuEntries.add( getString(R.string.menu_ref) );
 		
-		//Log.d("Goose", String.valueOf( android.os.Environment.getExternalStorageState() ) );
-		//SharedPreferences prefs = getSharedPreferences(GooseWidget.PREFS_NAME, 0);
+		SharedPreferences prefs = getSharedPreferences(GooseWidget.PREFS_NAME, 0);
+		page = prefs.getInt("comicPage", 0);
 		
-		menuEntries[1] = getString(R.string.menu_view);
-		menuEntries[2] = getString(R.string.menu_go);
-		menuEntries[3] = getString(R.string.menu_feed);
-		menuEntries[4] = getString(R.string.menu_dev);
-		menuEntries[5] = getString(R.string.menu_prev);
-		menuEntries[6] = getString(R.string.menu_next);
-		// + " (" + ( prefs.getInt("comicPage", 0 ) + 1 ) + "/" + ( prefs.getInt("availPages", 1 ) ) + ")";
+		
+		menuEntries.add( getString(R.string.menu_view) );
+		menuEntries.add( getString(R.string.menu_go) );
+		menuEntries.add( getString(R.string.menu_feed) );
+		menuEntries.add( getString(R.string.menu_dev) );
+		
+		if( page+1 < prefs.getInt("availPages", 0) )
+		{
+			menuEntries.add( getString(R.string.menu_prev) );
+		}
+		
+		if( page > 0 )
+		{
+			menuEntries.add( getString(R.string.menu_next) );
+		}
 		
 		super.onCreate(savedInstanceState);
-		setListAdapter(new ArrayAdapter<String>(this,R.layout.menu_item, menuEntries));
-		
+		setListAdapter(new ArrayAdapter<String>(this,R.layout.menu_item, menuEntries));		
 		
 
 	}
-
-	static int page = 0;
 	
 	@Override
 	protected void onListItemClick(android.widget.ListView l, android.view.View v, int position, long id )
@@ -45,7 +56,6 @@ public class GooseMenu extends ListActivity
 
 		SharedPreferences prefs = getSharedPreferences(GooseWidget.PREFS_NAME, 0);
 		SharedPreferences.Editor ed = prefs.edit();
-		
 
 		Uri uri;
 		switch( position )
@@ -62,23 +72,7 @@ public class GooseMenu extends ListActivity
 				prefs = getSharedPreferences(GooseWidget.PREFS_NAME, 0);
 				String path = prefs.getString("imagePath", "");
 				
-				Log.d("Goose", path);
-				
 				uri = Uri.parse(path);
-				
-				//File f = new File(super.getFilesDir()+"/goose_today.png");
-				
-				/*ContentValues values = new ContentValues(3);
-				values.put(Media.DISPLAY_NAME, "Goose");
-				values.put(Media.DESCRIPTION, "Today's Goose");
-				values.put(Media.MIME_TYPE, "image/png");
-				
-				Uri imageUri = getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, values);
-				
-				OutputStream outStream = getContentResolver().openOutputStream(imageUri);
-				outStream.write( f. )
-			    sourceBitmap.compress(Bitmap.CompressFormat.JPEG, 50, outStream);
-			    outStream.close();*/
 				
 				Intent pending = new Intent(Intent.ACTION_VIEW, uri);
 				startActivity(pending);
@@ -105,36 +99,49 @@ public class GooseMenu extends ListActivity
 			this.finish();
 			break;
 		case 5:
-			page++;
-			
-			ed = prefs.edit();
-			ed.putInt("comicPage", page );
-			ed.commit();
-			
+			if( menuEntries.get(5).equals(getString(R.string.menu_next)) )
+			{
+				DecPage();
+			}
+			else
+			{
+				IncPage();
+			}
 			this.startService(new Intent(this, UpdateService.class));
 			this.finish();
 			break;
 		case 6:
-			page--;
-			
-			ed.putInt("comicPage", page );
-			ed.commit();
-			
+			DecPage();
 			this.startService(new Intent(this, UpdateService.class));
 			this.finish();
 			break;
 		}
 	}
-		/*Uri uri = ContentUris.withAppendedId(getIntent().getData(), id);
+	
+	void DecPage()
+	{
+		SharedPreferences prefs = getSharedPreferences(GooseWidget.PREFS_NAME, 0);
+		SharedPreferences.Editor ed = prefs.edit();
+		
+		if( page > 0 )
+		{
+			page--;
+			ed.putInt("comicPage", page );
+			ed.commit();
+		}	
+	}
+	
+	void IncPage()
+	{
+		SharedPreferences prefs = getSharedPreferences(GooseWidget.PREFS_NAME, 0);
+		SharedPreferences.Editor ed = prefs.edit();
 
-        String action = getIntent().getAction();
-        if (Intent.ACTION_PICK.equals(action) || Intent.ACTION_GET_CONTENT.equals(action)) {
-            // The caller is waiting for us to return a note selected by
-            // the user.  The have clicked on one, so return it now.
-            setResult(RESULT_OK, new Intent().setData(uri));
-        } else {
-            // Launch activity to view/edit the currently selected item
-            startActivity(new Intent(Intent.ACTION_EDIT, uri));
-        }*/	
-	//}
+		if( page + 1 < prefs.getInt("availPages", 0 ) )
+		{
+			page++;
+			ed = prefs.edit();
+			ed.putInt("comicPage", page );
+			ed.commit();
+		}
+	}
 }
